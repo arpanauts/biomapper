3f64c1532f084880a47e0edb6ec28c163b5285f8
Trent Leslie <trentleslie@gmail.com>
2025-03-21 20:39:39 +0000

    update version

--- biomapper/__init__.py
+++ biomapper/__init__.py
@@ -20,7 +20,7 @@ from .monitoring.langfuse_tracker import LangfuseTracker
 from .standardization import RaMPClient
 from .core import SetAnalyzer
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __all__ = [
     # Core mapping
     "MetaboliteNameMapper",
